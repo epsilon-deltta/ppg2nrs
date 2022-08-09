d22870097536d9fc822d1666b0c459ec189451fe
epsilon-deltta <kokomong1316@gmail.com>
2022-08-10 03:00:53 +0900

    preprocessing functions for ppg

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-def num2class(nrs,type_=2):
+def num2class(nrs,type_=2,no_mild=[0,1,2,3]):
     nrs = int(nrs)
     label = ''
     if type_ ==2:
-        if nrs in [0,1,2,3]:
+        if nrs in no_mild:
             label = 0
         else:
             label = 1
