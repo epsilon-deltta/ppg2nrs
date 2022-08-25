7cf6576f2b111ccc498611b5b13af3fa1a60d0d1
epsilon-deltta <kokomong1316@gmail.com>
2022-08-26 08:02:26 +0900

    feat(class weight func): related to #6

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 import torch
-def train(dl,model,lossf,opt,device='cuda'):
+def train(dl,model,lossf,opt,device='cuda',class_weight=False):
+    
+    if class_weight: 
+        lossf.weight = class_weight
+        
     model.train()
     for x,y in dl:
         x,y = x.to(device),y.to(device)
@@ -31,11 +35,15 @@ def test(dl,model,lossf,epoch=None,exist_acc=True,device='cuda'):
     return accuracy,val_loss
 
 import copy
-def run(trdl, valdl, model, loss, opt, epoch=100,patience = 5, exist_acc=True, device='cuda'):
+def trainval(trdl, valdl, model, loss, opt, class_weight=False, epoch=100,patience = 5, exist_acc=True, device='cuda'):
     val_losses = {0:1}
     model = model.to(device)
+    
+    if class_weight:
+        class_weight = get_classweight(trdl)
+        
     for i in range(epoch):
-        train(trdl,model,loss,opt,device=device)
+        train(trdl,model,loss,opt,device=device,class_weight=class_weight)
         acc,val_loss = test(valdl,model,loss,epoch=i,exist_acc=exist_acc,device=device)
 
 
@@ -45,3 +53,17 @@ def run(trdl, valdl, model, loss, opt, epoch=100,patience = 5, exist_acc=True, d
         if i == min(val_losses,key=val_losses.get)+patience:
             break
     return best_model, val_losses
+
+import numpy as np
+def get_classweight(trdl):
+    
+    labels = []
+    for i in range(len(trdl.dataset)):
+        _,y = trdl.dataset[i]
+        labels.append(y)
+    labels = np.array(labels)
+    
+    from sklearn.utils import class_weight
+    class_weights=class_weight.compute_class_weight('balanced',classes=np.unique(labels),y=labels)
+    class_weights=torch.tensor(class_weights,dtype=torch.float)
+    return class_weights
